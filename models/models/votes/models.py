ae762c0974208806ee88f0dc9a0f3545c2a50c39
Nikita Smirnov <nvsmrnv@gmail.com>
2025-02-20 11:22:17 +0300

    update votes: new view and signals

--- models/models/votes/models.py
+++ models/models/votes/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
 
 from models.models.users.models import User
 from models.models.posts.models import Post
@@ -27,3 +29,19 @@ class Vote(Base):
 
     def __str__(self) -> str:
         return f'{self.user.username} проголосовал за {self.post.title}'
+
+
+@receiver(post_save, sender=Vote)
+def increase_votes_number(sender, instance, created, **kwargs):
+    if created:
+        post = instance.post
+        post.votes_number += 1
+        post.save()
+
+
+@receiver(post_delete, sender=Vote)
+def decrease_votes_number(sender, instance, **kwargs):
+    post = instance.post
+    post.votes_number -= 1
+    post.save()
+    
