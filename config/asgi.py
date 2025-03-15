77c3e2c9b5c15f5a9ed3871e07dca876f0ff7db8
Nikita Smirnov <nvsmrnv@gmail.com>
2025-03-16 00:25:39 +0300

    refactor ws

--- config/asgi.py
+++ config/asgi.py
@@ -1,11 +1,11 @@
 import os
+
+from channels.layers import get_channel_layer
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.layers import get_channel_layer
-
-from ws.routing import websocket_urlpatterns
 from ws.middlewares import JWTAuthMiddleware
+from ws.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
--- ws/middlewares.py
+++ ws/middlewares.py
@@ -1,10 +1,9 @@
 from urllib.parse import parse_qs
-from django.contrib.auth.models import AnonymousUser
-
-from rest_framework_simplejwt.tokens import AccessToken
 
 from channels.db import database_sync_to_async
 from channels.middleware import BaseMiddleware
+from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.tokens import AccessToken
 
 from models.models.users.models import User
 
