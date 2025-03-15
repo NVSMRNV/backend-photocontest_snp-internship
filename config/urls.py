b247a6dda15baa951ba37405355ebb0aa705371c
Nikita Smirnov <nvsmrnv@gmail.com>
2025-03-16 00:29:56 +0300

    add custom admin

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
 
 from config import settings
-
 
 urlpatterns = [
    path('api/', include('api.urls')),
--- models/admin.py
+++ models/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.utils.safestring import mark_safe
 from django_fsm import TransitionNotAllowed
 
+from models.models.comments.models import Comment
 from models.models.posts.models import Post
 from models.models.users.models import User
-from models.models.comments.models import Comment
 from models.models.votes.models import Vote
 
 
@@ -16,6 +16,16 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = (
         'title',
     )
+    fields = [
+        'id',
+        'title',
+        'author',
+        'image_thumbnail',
+        'image',
+        'status',
+        'votes_number',
+        'comments_number',
+    ]
     list_display = (
         'id',
         'image_thumbnail',
@@ -33,10 +43,30 @@ class PostAdmin(admin.ModelAdmin):
         'approve_posts',
         'pending_posts',
     ]
+    readonly_fields = (
+        'id',
+        'votes_number',
+        'comments_number',
+        'image_thumbnail',
+    )
 
     def image_thumbnail(self, post: Post):
-        return mark_safe(f'<img src="{post.image_thumbnail.url}" width="50" height="50" />')
+        return mark_safe(f'<img src="{post.image_thumbnail.url}" width="150" height="150" />')
     image_thumbnail.short_description = 'Фото'
+
+    def change_post_status(self, request, queryset, action):
+        for post in queryset:
+            try:
+                # получить метод нада и вызвать его
+                # getattr?
+                post.save()
+            except TransitionNotAllowed:
+                return
+            
+    # def reject_posts(self, request, queryset):
+    #     self.change_post_status(request, queryset, 'to_state_rej')
+    # reject_posts.short_description = 'Отклонить выбранные посты'
+
 
     def reject_posts(self, request, queryset):
         for post in queryset:
